5eb5c5da7fd67ba24ecc39eabc47f051d65dd409
Ryan Mitchell <ryan@thoughtcollective.com>
2021-08-19 11:19:57 +0100

    Simplify extension.php

--- Extension.php
+++ Extension.php
@@ -3,18 +3,9 @@
 namespace Miorder\Paymentsense;
 
 use System\Classes\BaseExtension;
-use Admin\Models\Payments_model;
 
-/**
- * paymentsense Extension Information File
- */
 class Extension extends BaseExtension
 {
-    /**
-     * Returns information about this extension.
-     *
-     * @return array
-     */
     public function registerPaymentGateways()
     {
         return [
@@ -34,56 +25,6 @@ class Extension extends BaseExtension
             'description' => 'Integration with Payment Sense Gateway',
             'icon' => 'fa-plug',
             'version' => '1.0.0'
-        ];
-    }
-
-    /**
-     * Register method, called when the extension is first registered.
-     *
-     * @return void
-     */
-    public function register()
-    {
-
-    }
-
-    /**
-     * Boot method, called right before the request route.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-       //$gateWay = Payments_model::select('data')->where('class_name', '=', 'MiOrder\PaymentSense\PaymentSense')->first();
-      //($gateWay->transaction_mode);
-    }
-
-    /**
-     * Registers any front-end components implemented in this extension.
-     *
-     * @return array
-     */
-    public function registerComponents()
-    {
-        return [
-// Remove this line and uncomment the line below to activate
-//            'Miorder\Paymentsense\Components\MyComponent' => 'myComponent',
-        ];
-    }
-
-    /**
-     * Registers any admin permissions used by this extension.
-     *
-     * @return array
-     */
-    public function registerPermissions()
-    {
-// Remove this line and uncomment block to activate
-        return [
-//            'Miorder.Paymentsense.SomePermission' => [
-//                'description' => 'Some permission',
-//                'group' => 'module',
-//            ],
         ];
     }
 }
